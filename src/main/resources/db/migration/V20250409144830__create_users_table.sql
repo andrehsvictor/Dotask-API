150f99e3f67172bd6106f850cd4f00d104aa9663
andrehsvictor <andrehsvictor@gmail.com>
2025-04-09 14:54:34 -0300

    feat: create users table in the database

---
+++ src/main/resources/db/migration/V20250409144830__create_users_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    "password" VARCHAR(255) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email_verified BOOLEAN NOT NULL DEFAULT FALSE,
+    email_verification_token VARCHAR(255) UNIQUE,
+    email_verification_token_expires_at TIMESTAMP WITH TIME ZONE,
+    password_reset_token VARCHAR(255) UNIQUE,
+    password_reset_token_expires_at TIMESTAMP WITH TIME ZONE,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP WITH TIME ZONE,
+);
