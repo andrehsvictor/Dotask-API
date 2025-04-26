d82bb9e8bd608bf37473f1cf70d7d43a9d344cef
andrehsvictor <andrehsvictor@gmail.com>
2025-04-26 11:13:54 -0300

    fix: update query parameter to be optional in findAll method

--- src/main/java/andrehsvictor/dotask/task/TaskController.java
+++ src/main/java/andrehsvictor/dotask/task/TaskController.java
@@ -32,7 +32,7 @@ public class TaskController {
 
     @GetMapping("/api/v1/tasks")
     public ResponseEntity<Page<GetTaskDto>> findAll(
-            @RequestParam(name = "q", defaultValue = "null") String query,
+            @RequestParam(name = "q", required = false) String query,
             TaskStatus status,
             TaskPriority priority,
             LocalDate startDate,
