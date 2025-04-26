6e4cd441e72c73cb7be09047bf1f1e69d3fd6509
andrehsvictor <andrehsvictor@gmail.com>
2025-04-26 15:40:17 -0300

    refactor: improve null and blank string handling in normalize method

--- src/main/java/andrehsvictor/dotask/util/StringNormalizer.java
+++ src/main/java/andrehsvictor/dotask/util/StringNormalizer.java
@@ -3,11 +3,13 @@ package andrehsvictor.dotask.util;
 public class StringNormalizer {
 
     public static String normalize(String str) {
-        if (str == null || str.isBlank()) {
+        if (str != null && str.isBlank()) {
+            return null;
+        }
+        if (str == null) {
             return null;
         }
         return str.trim()
-                .strip()
-                .toLowerCase();
+                .strip();
     }
 }
