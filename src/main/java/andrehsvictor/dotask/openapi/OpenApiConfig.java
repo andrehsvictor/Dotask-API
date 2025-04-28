2e438966d191115b01738ae9b4c40138ef2bad41
andrehsvictor <andrehsvictor@gmail.com>
2025-04-28 17:20:39 -0300

    feat: add OpenApiConfig class for API documentation configuration

---
+++ src/main/java/andrehsvictor/dotask/openapi/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package andrehsvictor.dotask.openapi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("Dotask API")
+                        .description("API for task management system"));
+    }
+}
